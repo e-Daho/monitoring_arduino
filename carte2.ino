ebecfcad81cd7114cb26826027dd30397e74a7d2
Thomas PERROT <daho@Daho-PC.fr>
2016-01-18 20:58:40 +0100

    Premier commit

---
+++ carte2.ino
@@ -0,0 +1,395 @@
+//ATOMIX Monitoring Carte 2
+
+//24/04/2015 v1.3 JGR
+//23/04/2015 v1.2 VGE delay enlevé
+//22/04/2015 v1.0 TPT récupération des données issues du calculateur et de la boite de vitesse
+//12/04/2015 v0.3 VGE mise a jour des id canbus et des infos calculo
+//31/03/2015 v0.2 VGE
+
+
+//--------------  assignation des pins  ------------------
+const int btn_1 = 49;
+const int btn_2 = 43;
+const int btn_3 = 47;
+const int btn_4 = 45;
+const int led_homing = 41;
+const int GND_1 = 39;
+
+const int in_0 = 27;
+const int in_1 = 25;
+const int in_2 = 33;
+const int in_3 = 31;
+const int in_4 = 23;
+const int out_1 = 37;
+const int out_2 = 29;
+const int GND_2 = 35;
+
+const int out_rapport = 3;
+const int shift_cut = 5;
+
+//-------------------  données calcul  ----------------
+boolean etat_btn_1;
+boolean etat_btn_1_prec;
+boolean etat_btn_2;
+boolean etat_btn_2_prec;
+boolean etat_btn_3;
+boolean etat_btn_3_prec;
+boolean etat_btn_4;
+boolean etat_btn_4_prec;
+boolean led_clignotante;
+boolean led_force;
+boolean homed;
+boolean error;
+long date_clignotement;
+long date_coupure;
+int delai_clignotement = 200;
+int temps_coupure = 400;
+const long refresh_can = 250;
+
+//--------------------  variables stockage  -------------------
+
+int id;
+int rapport_recherche;
+int rapport_engage;
+boolean positions[16][4];
+long temps_can;
+
+int regime;
+int rapport = 3;
+
+//initialisation Canbus
+#include <SPI.h>
+#include "mcp_can.h"
+MCP_CAN CAN(53);
+
+unsigned char Flag_Recv = 0;
+unsigned char len = 0; //à vérifier
+unsigned char buf[8];
+char str[20];
+
+//variables mixou
+
+void setup() {
+  Serial.begin(115200);
+  
+    //initialisation canbus
+  
+START_INIT:
+  if(CAN_OK == CAN.begin(CAN_1000KBPS))  
+  {
+    Serial.println("CAN BUS Shield init ok!");
+  }
+  else
+  {
+    Serial.println("CAN BUS Shield init fail");
+    Serial.println("Init CAN BUS Shield again");
+    delay(100);
+    goto START_INIT;
+  }
+
+date_clignotement = millis();
+  led_clignotante = true;
+  led_force = false;
+  homed = false;
+  error = false;
+  
+  rapport_recherche = 0;    
+  rapport_engage = rapport_recherche;
+  
+  pinMode(btn_1, INPUT);
+  pinMode(btn_2, INPUT);
+  pinMode(btn_3, INPUT);
+  pinMode(btn_4, INPUT);  
+  pinMode(led_homing, OUTPUT);
+  pinMode(GND_1, OUTPUT);
+  
+  digitalWrite(btn_1, HIGH);
+  digitalWrite(btn_2, HIGH);  
+  digitalWrite(btn_3, HIGH);
+  digitalWrite(btn_4, HIGH);
+  digitalWrite(led_homing, LOW);
+  digitalWrite(GND_1, LOW);
+  
+  pinMode(in_0, OUTPUT);
+  pinMode(in_1, OUTPUT);
+  pinMode(in_2, OUTPUT);
+  pinMode(in_3, OUTPUT);
+  pinMode(in_4, OUTPUT);
+  pinMode(out_1, INPUT);
+  pinMode(out_2, INPUT);
+  pinMode(GND_2, OUTPUT);
+    
+  digitalWrite(in_0, LOW);
+  digitalWrite(in_1, LOW);
+  digitalWrite(in_2, LOW);
+  digitalWrite(in_3, LOW);
+  digitalWrite(in_4, LOW);  
+  digitalWrite(GND_2, LOW);
+  
+  pinMode(out_rapport, OUTPUT);
+  
+  pinMode(shift_cut, OUTPUT);  
+  digitalWrite(shift_cut, HIGH);
+  
+  
+  etat_btn_1_prec = HIGH;
+  etat_btn_2_prec = HIGH;
+    
+  positions[0][0] = 0;
+  positions[0][1] = 0;
+  positions[0][2] = 0;
+  positions[0][3] = 0;  
+  
+  positions[1][0] = 1;
+  positions[1][1] = 0;
+  positions[1][2] = 0;
+  positions[1][3] = 0;
+  
+  positions[2][0] = 0;
+  positions[2][1] = 1;
+  positions[2][2] = 0;
+  positions[2][3] = 0;
+  
+  positions[3][0] = 1;
+  positions[3][1] = 1;
+  positions[3][2] = 0;
+  positions[3][3] = 0;
+  
+  positions[4][0] = 0;
+  positions[4][1] = 0;
+  positions[4][2] = 1;
+  positions[4][3] = 0;
+  
+  positions[5][0] = 1;
+  positions[5][1] = 0;
+  positions[5][2] = 1;
+  positions[5][3] = 0;
+  
+  positions[6][0] = 0;
+  positions[6][1] = 1;
+  positions[6][2] = 1;
+  positions[6][3] = 0;
+  
+  positions[7][0] = 1;
+  positions[7][1] = 1;
+  positions[7][2] = 1;
+  positions[7][3] = 0;
+  
+  positions[8][0] = 0;
+  positions[8][1] = 0;
+  positions[8][2] = 0;
+  positions[8][3] = 1;
+  
+  positions[9][0] = 1;
+  positions[9][1] = 0;
+  positions[9][2] = 0;
+  positions[9][3] = 1;
+  
+  positions[10][0] = 0;
+  positions[10][1] = 1;
+  positions[10][2] = 0;
+  positions[10][3] = 1;
+  
+  positions[11][0] = 1;
+  positions[11][1] = 1;
+  positions[11][2] = 0;
+  positions[11][3] = 1;
+  
+  positions[12][0] = 0;
+  positions[12][1] = 0;
+  positions[12][2] = 1;
+  positions[12][3] = 1;
+  
+  positions[13][0] = 1;
+  positions[13][1] = 0;
+  positions[13][2] = 1;
+  positions[13][3] = 1;
+  
+  positions[14][0] = 0;
+  positions[14][1] = 1;
+  positions[14][2] = 1;
+  positions[14][3] = 1;
+  
+  positions[15][0] = 1;
+  positions[15][1] = 1;
+  positions[15][2] = 1;
+  positions[15][3] = 1;
+ 
+}
+
+
+//-----------------------------------------------
+//-------------------  Loop ---------------------
+
+void loop() {
+  
+etat_btn_1 = digitalRead(btn_1);
+
+  if(etat_btn_1 != etat_btn_1_prec)
+  {
+    if(!etat_btn_1)
+    {
+      action_btn_1();
+      Serial.write("bouton 1");
+    }
+    etat_btn_1_prec = !etat_btn_1_prec;
+  }
+
+  etat_btn_2 = digitalRead(btn_2);
+
+  if(etat_btn_2 != etat_btn_2_prec)
+  {
+    if(!etat_btn_2)
+    {
+      action_btn_2();
+      Serial.write("bouton 2");
+    }
+    etat_btn_2_prec = !etat_btn_2_prec;
+  }
+  
+  etat_btn_3 = digitalRead(btn_3);
+
+  if(etat_btn_3 != etat_btn_3_prec)
+  {
+    if(!etat_btn_3)
+    {
+      action_btn_3();
+      Serial.write("bouton 3");
+    }
+    etat_btn_3_prec = !etat_btn_3_prec;
+  }
+
+  etat_btn_4 = digitalRead(btn_4);
+
+  if(etat_btn_4 != etat_btn_4_prec)
+  {
+    if(!etat_btn_4)
+    {
+      action_btn_4();
+      Serial.write("bouton 3");
+    }
+    etat_btn_4_prec = !etat_btn_4_prec;
+  }
+  
+  engager_rapport(rapport_recherche);
+  
+  
+  if(true) //if(moteur électrique stoppé)
+  {
+    rapport_engage = rapport_recherche;
+  }
+  
+  if(!digitalRead(out_1) && !digitalRead(out_2))
+  {
+    error = true;
+    homed = false;
+    delai_clignotement = 500;
+    led_clignotante = true;
+    led_force = false;
+  }
+  
+  else
+  {  
+    error = false;    
+    delai_clignotement = 200;
+    led_clignotante = false;
+    led_force = false;
+  }
+  
+  analogWrite(out_rapport, max(0, rapport_engage-2)*255/6);
+  
+  clignoter_led();
+  
+  if(millis() - date_coupure > temps_coupure)
+    digitalWrite(shift_cut, HIGH);
+  
+//envoyer CANBUS
+if(millis()>=temps_can + refresh_can){
+unsigned char stmp[8] = {rapport_engage,0,0,0,0,0,0,0};  
+  // send data:  id = 0x00, standard flame, data len = 8, stmp: data buf
+  CAN.sendMsgBuf(20, 0, 8, stmp);
+temps_can = millis();  
+}
+}
+
+void clignoter_led()
+{
+  
+  if(millis() - date_clignotement > delai_clignotement)
+    date_clignotement = millis();
+  
+  if(millis() - date_clignotement < (delai_clignotement/2) && led_clignotante)
+  {
+    digitalWrite(led_homing, HIGH);
+  }
+  else
+  {
+    if(!led_force)
+      digitalWrite(led_homing, LOW);
+    else
+      digitalWrite(led_homing, HIGH);
+  }
+  
+}
+
+void engager_rapport(int rapport)
+{
+
+  digitalWrite(in_0, LOW);
+  digitalWrite(in_1, positions[rapport][0]);
+  digitalWrite(in_2, positions[rapport][1]);
+  digitalWrite(in_3, positions[rapport][2]);
+  digitalWrite(in_4, positions[rapport][3]); 
+  
+}
+
+void action_btn_1()
+{
+  if(rapport_recherche > 3)
+  {
+    rapport_recherche--;
+  }
+}
+
+void action_btn_2()
+{
+  if(millis() > 1000)
+  {
+    if(rapport_recherche < 8 && rapport_recherche > 1)
+    {
+      rapport_recherche++;
+      digitalWrite(shift_cut, LOW);
+      date_coupure = millis();
+    }
+    else if(rapport_recherche == 1)
+      rapport_recherche = 3;
+      
+    
+  }
+}
+
+void action_btn_3()
+{
+  if(error)
+  {
+    rapport_recherche = 0;
+  }
+  
+  else if (!error && millis() > 1000)
+  {  
+    if(rapport_recherche == 1)
+    {
+      rapport_recherche = 2;
+      delay(50);
+      rapport_recherche = 1; 
+    }
+    else
+      rapport_recherche = 1;
+  }
+  
+}
+
+void action_btn_4()
+{
+  rapport_recherche = 2;
+}
---
+++ carte3.ino
@@ -0,0 +1,295 @@
+/*ATOMIX Monitoring Carte 3
+
+ 15/04/2015 v1   TPT : correction de bugs, mise à jour des id des cartes
+ 14/04/2015 v0.6 VGE : mise au point des communications
+ 09/04/2015 v0.5 TPT : récupération des données du calculateur
+ 04/04/2015 v0.4 TPT : création automatique des fichiers sur la carte SD
+ 31/03/2015 v0.3 TPT : ajout du module de carte SD, stockage des variables sur la carte SD
+ 31/03/2015 v0.2 VGE : Récupération des données par le CANbus et les capteurs
+ 
+  
+ 	
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 8	 
+ */
+
+#include <SD.h>
+#include <SPI.h>
+#include "mcp_can.h"
+
+//--------------  assignation des pins  ------------------
+
+const int cpt_debat_arg = 0;
+const int cpt_debat_ard = 1;
+const int cpt_vit_arg = 2;
+const int cpt_vit_ard = 3;
+const int cpt_press_ar = 2;
+
+//-------------------  donnÃ©es calcul  ----------------
+const int diametre = 440;
+const int nbr_trous = 24;
+const int limite_rafraich = 12;
+
+//--------------------  variables stockage  -------------------
+float vitesse_arg ;
+float vitesse_ard ;
+int vitesse_avg ;
+int vitesse_avd ;
+
+int cpt_vitesse_g ;
+int cpt_vitesse_d ;
+long temps_g;
+long temps_gdif;
+long temps_d;
+long temps_ddif;
+
+int debat_arg ;
+int debat_ard ;
+int debat_avg ;
+int debat_avd ;
+int debat_cre ;
+
+int press_av;
+int press_ar;
+
+int regime;
+int rapport;
+int temperature;
+
+long temps;
+
+int id;
+
+//--------------------  initialisation Canbus  -------------------
+
+unsigned char Flag_Recv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
+MCP_CAN CAN(10);
+
+//--------------------  initialisation Shield SD  -------------------
+
+const int chipSelect = 8;
+File myFile;
+char datafile[11];
+Sd2Card card;
+int numero = 0;
+
+/*----------------------------------------------
+ --------------------  Setup  -------------------
+ ----------------------------------------------*/
+
+void setup()
+{
+  Serial.begin(115200);
+
+  //initialisation des pins
+  pinMode(cpt_vit_arg,INPUT);
+  pinMode(cpt_vit_ard,INPUT);
+  //on active les résistances pull-up
+  digitalWrite(cpt_vit_arg, HIGH);
+  digitalWrite(cpt_vit_ard, HIGH);
+  //on définit les interruption
+  attachInterrupt(0,inter_vitesse_gauche,FALLING);
+  attachInterrupt(1,inter_vitesse_droite,FALLING);
+
+  temps_g=millis();
+  temps_d=millis();
+
+  //initialisation du shield SD
+START_INIT_SD:  
+
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+
+  // see if the card is present and can be initialized:
+  if (SD.begin(chipSelect)){
+    Serial.println("SD Shield init ok!");
+  }
+  else
+  {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    delay(100);
+    goto START_INIT_SD;
+  }
+
+  //initialisation du shield CANbus
+START_INIT_CAN:
+
+  Serial.print("Initializing CAN Shield...");
+  if(CAN_OK == CAN.begin(CAN_1000KBPS))                   // init can bus : baudrate = 1000k
+  {
+    Serial.println("CAN BUS Shield init ok!");
+  }
+  else
+  {
+    Serial.println("CAN BUS Shield init fail");
+    delay(100);
+    goto START_INIT_CAN;
+  }
+
+  //Creation d'un nouveau fichier d'enregistrement
+  
+  do{
+    sprintf(datafile,"data%d.txt",numero);  
+    numero++;
+  }
+  while(SD.exists(datafile));
+  Serial.print(datafile);  
+  Serial.println(" doesn't exist. Creation...");
+  myFile = SD.open(datafile, FILE_WRITE);
+  myFile.close();
+}
+
+
+/*----------------------------------------------
+ ----------------  Interruption  ---------------
+ ----------------------------------------------*/
+
+void inter_vitesse_gauche() {
+  cpt_vitesse_g++ ; 
+}
+
+void inter_vitesse_droite() {
+  cpt_vitesse_d++ ;
+}
+
+/*----------------------------------------------
+ --------------------  Loop  -------------------
+ ----------------------------------------------*/
+
+void loop()
+{
+ if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+asm volatile ("  jmp 0"); 
+ }
+ else{
+
+  //--------------------  Stockage des données dans les variables  -------------------
+
+  debat_arg=analogRead(cpt_debat_arg);
+  debat_ard=analogRead(cpt_debat_ard);
+  press_ar=analogRead(cpt_press_ar);
+
+ if(cpt_vitesse_g >= limite_rafraich){
+    temps_gdif = millis()-temps_g;
+    temps_g = millis();  
+    vitesse_arg = 3.14*diametre*3.6/((nbr_trous/limite_rafraich)*temps_gdif);
+    cpt_vitesse_g=0;
+    }
+    
+      
+  if(cpt_vitesse_d >= limite_rafraich){
+    temps_ddif = millis()-temps_d;
+    temps_d = millis();   
+    vitesse_ard = 3.14*diametre*3.6/((nbr_trous/limite_rafraich)*temps_ddif);
+    cpt_vitesse_d=0;
+    }
+    
+ if(millis()>=temps_d+1000){
+   vitesse_ard = 0;
+ }
+ 
+ if(millis()>=temps_g+1000){
+   vitesse_arg = 0;
+ }
+
+  //--------------------  Réception des données du CANbus  -------------------  
+  for(int i=0; i<3; i++){
+  if(CAN_MSGAVAIL == CAN.checkReceive())      // check if data coming
+  {  
+    CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+    id = CAN.getCanId();
+    Serial.println(id);
+    if(id==10)
+    {
+      vitesse_avg = buf[0];
+      vitesse_avd = buf[1];
+      debat_avg = buf[2]*4+buf[3];
+      debat_avd = buf[4]*4+buf[5];
+      debat_cre = buf[6]*4;
+      press_av = buf[7]*4;
+    }
+
+    if(id==20)
+    {
+      rapport = buf[0];
+    }
+
+    if(id==8192)
+    {
+      regime = buf[0];
+      temperature =buf[4];
+    }
+
+  }
+ }
+ 
+  temps=millis();
+
+  File myFile = SD.open(datafile, FILE_WRITE);   
+  if (myFile) {
+    myFile.print(temps);
+    myFile.print("\t");
+    myFile.print(vitesse_arg);
+    myFile.print("\t");
+    myFile.print(vitesse_ard);
+    myFile.print("\t");
+    myFile.print(vitesse_avg);
+    myFile.print("\t");
+    myFile.print(vitesse_avd);
+    myFile.print("\t");
+    myFile.print(debat_arg);
+    myFile.print("\t");
+    myFile.print(debat_ard);
+    myFile.print("\t");
+    myFile.print(debat_avg);
+    myFile.print("\t");
+    myFile.print(debat_avd);
+    myFile.print("\t");
+    myFile.print(debat_cre);
+    myFile.print("\t");
+    myFile.print(press_av);
+    myFile.print("\t");
+    myFile.print(press_ar);
+    myFile.print("\t");
+    myFile.print(regime);
+    myFile.print("\t");
+    myFile.print(rapport);
+    myFile.print("\t");
+    myFile.print(temperature);
+    myFile.println(""); 
+    myFile.close();    
+  }
+
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening the file");
+  } 
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
